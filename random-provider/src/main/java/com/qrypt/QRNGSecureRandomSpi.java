47d9881b5316c0b5bd0592c3b99bc6df376ff83d
kenneth-huebsch <kenneth.huebsch@gmail.com>
2024-08-22 21:06:44 +0000

    added sha512

--- random-provider/src/main/java/com/qrypt/QRNGSecureRandomSpi.java
+++ random-provider/src/main/java/com/qrypt/QRNGSecureRandomSpi.java
@@ -1,12 +1,53 @@
 package com.qrypt;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandomSpi;
 
 public class QRNGSecureRandomSpi extends SecureRandomSpi {
 
+    private byte[] SHA512Hash(byte[] bytes){
+        final int SHA512_RETURN_LENGTH = 64;
+
+        // List to store all hashed chunks
+        byte[] returnValue = new byte[bytes.length];
+
+        try {
+            // Get SHA-512 MessageDigest instance
+            MessageDigest sha512 = MessageDigest.getInstance("SHA-512");
+
+            // Process the input in chunks of 64 bytes
+            for (int i = 0; i < bytes.length; i += SHA512_RETURN_LENGTH) {
+                // Calculate the chunk size (handle last chunk if smaller than 64 bytes)
+                int chunkSize = Math.min(SHA512_RETURN_LENGTH, bytes.length - i);
+                
+                // Copy the chunk
+                byte[] chunk = new byte[chunkSize];
+                System.arraycopy(bytes, i, chunk, 0, chunkSize);
+                
+                // Hash the chunk and add the result to the return value
+                byte[] hashedChunk = sha512.digest(chunk);
+                System.arraycopy(hashedChunk, 0, returnValue, i, chunkSize);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-512 algorithm not found", e);
+        }
+
+        return returnValue;
+    }
+
     @Override
     protected void engineSetSeed(byte[] seed) {
-        this.engineNextBytes(seed);
+        QRNGRestAPIClient apiClient = new QRNGRestAPIClient();
+        byte[] decodedBytes = apiClient.getRandom(seed.length);
+
+        // Test for discrepency in lengths
+        if (decodedBytes.length != seed.length){
+            System.out.println("Error: API returned bytes is a different size then the byte array");
+            // TODO - Fill decoded bytes with system random
+        }
+
+        System.arraycopy(decodedBytes, 0, seed, 0, decodedBytes.length);
     }
 
     @Override
@@ -20,13 +61,15 @@ public class QRNGSecureRandomSpi extends SecureRandomSpi {
             // TODO - Fill decoded bytes with system random
         }
 
+        decodedBytes = SHA512Hash(decodedBytes);
+
         System.arraycopy(decodedBytes, 0, bytes, 0, decodedBytes.length);
     }
 
     @Override
     protected byte[] engineGenerateSeed(int numBytes) {
         byte[] returnValue = new byte[numBytes];
-        this.engineNextBytes(returnValue);
+        this.engineSetSeed(returnValue);
 
         return returnValue;
     }
